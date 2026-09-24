a9de32c497a9afdb11e532b329d5eb922e5e258c
Yammery80 <yammery.28@gmail.com>
2026-09-24 14:48:46 -0600

    Ejemplo con Singlenton

--- singleton/configuraciones.dart
+++ singleton/configuraciones.dart
@@ -1,5 +1,17 @@
-class configuracion {
-  String? idioma;
+class Configuration {
+  
+  //Constructor privado
+  Configuration._();
 
-  configuracion(this.idioma);
+  // Lo que es static: vive durante todo el ciclo de la app
+  //final: no puede cambiar la referencia de a variable, pero si puede cambiar el valor de sus propiedades
+  static final Configuration _instancia = Configuration._();
+
+  
+  //Es un constructor de fábrica que devueve a misma instancia
+  factory Configuration() => _instancia;
+
+  String idioma = 'es';
+
+  
 }
--- singleton/main.dart
+++ singleton/main.dart
@@ -1,12 +1,18 @@
 import 'configuraciones.dart';
 
 void main() {
-  final pantallaInicio = configuracion('es');
-  final pantallaPerfil = configuracion('es');
+  final pantallaInicio = Configuration();
+  final pantallaPerfil = Configuration();
 
+  print('Idioma de la pantalla de inicio: ${pantallaInicio.idioma}');
+  print('Idioma de la pantalla de perfil: ${pantallaPerfil.idioma}');
 
   pantallaInicio.idioma = 'en';
 
   print('Idioma de la pantalla de inicio: ${pantallaInicio.idioma}');
   print('Idioma de la pantalla de perfil: ${pantallaPerfil.idioma}');
+  
+  print(identical(pantallaInicio, pantallaPerfil) // true
+    ? 'Ambas pantallas son la misma instancia (singleton).'
+    : 'Ambas pantallas son instancias diferentes.' );
 }
